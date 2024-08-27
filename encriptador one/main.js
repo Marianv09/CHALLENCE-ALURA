154e03f5cd9796b8c7ac02f72c2ca5367f199d0a
Marianv09 <kenianovas23@gmail.com>
2024-08-27 06:19:38 -0400

    Add files via upload

---
+++ encriptador one/main.js
@@ -0,0 +1,19 @@
+function encriptar() {
+    let texto = document.getElementById("textoEntrada").value;
+    let textoEncriptado = texto.replace(/e/g, "enter")
+                               .replace(/i/g, "imes")
+                               .replace(/a/g, "ai")
+                               .replace(/o/g, "ober")
+                               .replace(/u/g, "ufat");
+    document.getElementById("resultado").innerText = textoEncriptado;
+}
+
+function desencriptar() {
+    let texto = document.getElementById("textoEntrada").value;
+    let textoDesencriptado = texto.replace(/enter/g, "e")
+                                  .replace(/imes/g, "i")
+                                  .replace(/ai/g, "a")
+                                  .replace(/ober/g, "o")
+                                  .replace(/ufat/g, "u");
+    document.getElementById("resultado").innerText = textoDesencriptado;
+}
